08a06f729afb194167eba1b84276c183ec6bcaa8
quanla <quanla@mobio.vn>
2021-11-26 10:25:02 +0700

    commit

--- analytics/src/main/java/com/mobio/analytics/client/service/SDKPushFirebaseService.java
+++ analytics/src/main/java/com/mobio/analytics/client/service/SDKPushFirebaseService.java
@@ -1,8 +1,6 @@
-package com.mobio.sample;
+package com.mobio.analytics.client.service;
 
 import androidx.annotation.NonNull;
-import androidx.work.OneTimeWorkRequest;
-import androidx.work.WorkManager;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
@@ -14,8 +12,8 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.JSONTokener;
 
-public class MyFirebaseMessagingService extends FirebaseMessagingService {
-    public MyFirebaseMessagingService() {
+public class SDKPushFirebaseService extends FirebaseMessagingService {
+    public SDKPushFirebaseService() {
     }
 
     @Override
@@ -39,7 +37,7 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
             LogMobio.logD("MyFirebaseMessagingService", "Message data payload: " + remoteMessage.getData());
 
             try {
-                JSONObject json= (JSONObject) new JSONTokener(remoteMessage.getData().toString()).nextValue();
+                JSONObject json = (JSONObject) new JSONTokener(remoteMessage.getData().toString()).nextValue();
                 JSONObject json2 = json.getJSONObject("alert");
                 String title = (String) json2.get("title");
                 String body = (String) json2.get("body");
@@ -47,78 +45,46 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
                 LogMobio.logD("MyFirebaseMessagingService", "title: " + title);
                 LogMobio.logD("MyFirebaseMessagingService", "body: " + body);
 
-//                Intent i = new Intent();
-//                i.setAction(RECEIVE_NOTIFICATION_ACTION);
-//                i.putExtra(INTENT_KEY_TITLE, title);
-//                i.putExtra(INTENT_KEY_DETAIL, body);
-//                sendBroadcast(i);
-
                 NotiResponseObject notiResponseObject = new NotiResponseObject.Builder()
                         .withType(NotiResponseObject.TYPE_NATIVE).withContent(body)
                         .withData(body).withTitle(title)
                         .build();
 
-                if(title.contains("[Case demo 1]")){
+                if (title.contains("[Case demo 1]")) {
                     notiResponseObject.setDes_screen("Recharge");
                 }
 
                 Analytics.getInstance().showGlobalPopup(notiResponseObject);
             } catch (JSONException e) {
                 e.printStackTrace();
-            }
 
-            if (/* Check if data needs to be processed by long running job */ true) {
-                // For long-running tasks (10 seconds or more) use WorkManager.
-                scheduleJob();
-            } else {
-                // Handle message within 10 seconds
-                handleNow();
-            }
-
-            if (remoteMessage.getNotification() != null) {
-                LogMobio.logD("MyFirebaseMessagingService", "Message Notification Body: " + remoteMessage.getNotification().getBody());
+                if (remoteMessage.getNotification() != null) {
+                    LogMobio.logD("MyFirebaseMessagingService", "Message Notification Body: " + remoteMessage.getNotification().getBody());
 
 
+                    String title = remoteMessage.getNotification().getTitle();
+                    String detail = remoteMessage.getNotification().getBody();
 
-                String title = remoteMessage.getNotification().getTitle();
-                String detail = remoteMessage.getNotification().getBody();
 
+                    NotiResponseObject notiResponseObject = new NotiResponseObject.Builder()
+                            .withType(NotiResponseObject.TYPE_NATIVE).withContent(detail)
+                            .withData(detail).withTitle(title)
+                            .build();
 
-                NotiResponseObject notiResponseObject = new NotiResponseObject.Builder()
-                        .withType(NotiResponseObject.TYPE_NATIVE).withContent(detail)
-                        .withData(detail).withTitle(title)
-                        .build();
-
-                Analytics.getInstance().showGlobalPopup(notiResponseObject);
+                    Analytics.getInstance().showGlobalPopup(notiResponseObject);
 
 //            Intent i = new Intent();
 //            i.setAction(RECEIVE_NOTIFICATION_ACTION);
 //            i.putExtra(INTENT_KEY_TITLE, title);
 //            i.putExtra(INTENT_KEY_DETAIL, detail);
 //            sendBroadcast(i);
+                }
+
             }
 
+            // Also if you intend on generating your own notifications as a result of a received FCM
+            // message, here is where that should be initiated. See sendNotification method below.
         }
 
-        // Also if you intend on generating your own notifications as a result of a received FCM
-        // message, here is where that should be initiated. See sendNotification method below.
-    }
-
-    /**
-     * Schedule async work using WorkManager.
-     */
-    private void scheduleJob() {
-        // [START dispatch_job]
-        OneTimeWorkRequest work = new OneTimeWorkRequest.Builder(MyWorker.class)
-                .build();
-        WorkManager.getInstance(this).beginWith(work).enqueue();
-
-    }
-
-    /**
-     * Handle time allotted to BroadcastReceivers.
-     */
-    private void handleNow() {
-        LogMobio.logD("MyFirebaseMessagingService", "Short lived task is done.");
     }
 }
